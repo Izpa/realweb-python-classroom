e7c6531081a53fde08a90c9d442f4a699b717b08
Артем Марков <a.markov@realweb.ru>
2018-12-07 18:43:43 +0300

    move chrome driver for selenium

--- lesson00/selenium/main.py
+++ lesson00/selenium/main.py
@@ -3,7 +3,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome('/Users/izpa/Downloads/chromedriver')
+driver = webdriver.Chrome('./chromedriver')
 driver.get("http://google.com")
 element = driver.find_element_by_xpath("//input[@class='gLFyf gsfi']")
 element.send_keys("Котики")
